f427db79ccd0394928afd0299e7703993b6f5eca
Vadim Volkov <antibiotik15@ya.ru>
2021-04-11 22:39:00 +0300

    Refactoring

--- src/main/java/task/TicTacToe.java
+++ src/main/java/task/TicTacToe.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Objects;
 
 public class TicTacToe {
     private static final String[][] matrix = new String[3][3];
@@ -53,6 +54,10 @@ public class TicTacToe {
                     isComputerWrong = false;
                 }
             }
+        }
+
+        if (!hasWinner()) {
+            System.out.println("Draw!");
         }
     }
 
@@ -163,29 +168,29 @@ public class TicTacToe {
             return false;
         }
 
-        if (matrix[0][0].equals(matrix[1][1]) && matrix[0][0].equals(matrix[2][2])) {
+        if (matrix[0][0] != null && matrix[0][0].equals(matrix[1][1]) && matrix[0][0].equals(matrix[2][2])) {
             return true;
         }
-        if (matrix[0][2].equals(matrix[1][1]) && matrix[0][2].equals(matrix[2][0])) {
+        if (matrix[0][2] != null && matrix[0][2].equals(matrix[1][1]) && matrix[0][2].equals(matrix[2][0])) {
             return true;
         }
-        if (matrix[0][0].equals(matrix[0][1]) && matrix[0][1].equals(matrix[0][2])) {
+        if (matrix[0][0] != null && matrix[0][0].equals(matrix[0][1]) && matrix[0][1].equals(matrix[0][2])) {
             return true;
         }
-        if (matrix[1][0].equals(matrix[1][1]) && matrix[1][0].equals(matrix[1][2])) {
+        if (Objects.equals(matrix[1][0], matrix[1][1]) && matrix[1][0].equals(matrix[1][2])) {
             return true;
         }
-        if (matrix[2][0].equals(matrix[2][1]) && matrix[2][0].equals(matrix[2][2])) {
+        if (Objects.equals(matrix[2][0], matrix[2][1]) && matrix[2][0].equals(matrix[2][2])) {
             return true;
         }
-        if (matrix[0][0].equals(matrix[1][0]) && matrix[0][0].equals(matrix[2][0])) {
+        if (Objects.equals(matrix[0][0], matrix[1][0]) && matrix[0][0].equals(matrix[2][0])) {
             return true;
         }
-        if (matrix[0][1].equals(matrix[1][1]) && matrix[0][1].equals(matrix[2][1])) {
+        if (Objects.equals(matrix[0][1], matrix[1][1]) && matrix[0][1].equals(matrix[2][1])) {
             return true;
         }
 
-        return matrix[0][2].equals(matrix[1][2]) && matrix[0][2].equals(matrix[2][2]);
+        return Objects.equals(matrix[0][2], matrix[1][2]) && matrix[0][2].equals(matrix[2][2]);
     }
 
     private static String getComputerSign(String userSign) {
